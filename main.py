8bbbfe3fac46574aeb9c8ebb3746291664e3cc18
hkikoryan <123384859+hkikoryan@users.noreply.github.com>
2023-09-26 23:45:08 +0900

    Add files via upload
    change contents

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 # load_dotenv()
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
+
 chat_model = ChatOpenAI()
 
-st.title('인공지능 시인')
-st.title('_AI가 대신_ :blue[시를] 지어드립니다. :sunglasses:')
+st.title('광고문구 AI')
+st.title('_AI가 대신_ :blue[광고문구]를 생성해드립니다. :sunglasses:')
 
-content = st.text_input('시의 주제를 적어주세요.')
+content = st.text_input('원하는 광고 문구를 적어주세요.')
 
-if st.button('시 작성 요청하기'):
-    with st.spinner('시 작성중..'):
-        result = chat_model.predict(content + "에 대한 시를 써줘")
+if st.button('광고 문구 요청하기'):
+    with st.spinner('광고 문구 작성중..'):
+        result = chat_model.predict(content + "에 대한 짧은 광고 문구 5가지를 만들어줘")
         st.write(result)
+
 
 
