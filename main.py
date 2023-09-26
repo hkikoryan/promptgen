ea201f1e7c0321c8a9ded864188479c13169ec36
hkikoryan <123384859+hkikoryan@users.noreply.github.com>
2023-09-26 23:53:00 +0900

    Add files via upload
    st.title 변경

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
 
-st.title('광고카피 AI')
-st.title('AI가 :blue[광고카피]를 생성해드립니다. :sunglasses:')
+st.title('AI Copy Writer')
+st.title('AI가 당신을 위한 :blue[광고 문구]를 생성해드립니다. :sunglasses:')
 
 content = st.text_input('원하는 광고 문구를 적어주세요.')
 
-if st.button('광고 카피 요청하기'):
-    with st.spinner('광고 카피 작성중..'):
+if st.button('광고 문구 요청하기'):
+    with st.spinner('광고 문구 작성중..'):
         result = chat_model.predict(content + "에 대한 짧고 매력있는 광고 문구 5가지를 만들어줘")
         st.write(result)
 
