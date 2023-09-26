e56daa3557aeea6fd05a1c1a769e085ad26504ff
hkikoryan <123384859+hkikoryan@users.noreply.github.com>
2023-09-26 23:48:21 +0900

    Add files via upload
    광고문구 -> 광고카피

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
 
-st.title('광고문구 AI')
-st.title('_AI가 대신_ :blue[광고문구]를 생성해드립니다. :sunglasses:')
+st.title('광고카피 AI')
+st.title('AI가 :blue[광고카피]를 생성해드립니다. :sunglasses:')
 
 content = st.text_input('원하는 광고 문구를 적어주세요.')
 
-if st.button('광고 문구 요청하기'):
-    with st.spinner('광고 문구 작성중..'):
-        result = chat_model.predict(content + "에 대한 짧은 광고 문구 5가지를 만들어줘")
+if st.button('광고 카피 요청하기'):
+    with st.spinner('광고 카피 작성중..'):
+        result = chat_model.predict(content + "에 대한 짧고 매력있는 광고 문구 5가지를 만들어줘")
         st.write(result)
 
 
