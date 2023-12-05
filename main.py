e03c47af769ecd97f6f6bb80beeb99dfb473f393
hkikoryan <123384859+hkikoryan@users.noreply.github.com>
2023-12-05 10:53:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from googletrans import Translator
 import streamlit as st
 from PIL import Image
 from langchain.chat_models import ChatOpenAI
